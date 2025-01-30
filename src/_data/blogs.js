9d4b12870165a7d5bb1028c971c5d4e6f752cd95
jclee8888 <jeffreyclee@gmail.com>
2025-01-29 22:15:35 -0800

    css changes to blog

--- src/_data/blogs.js
+++ src/_data/blogs.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
-const API_URL = "https://demo.dotcms.com/api/v1/graphql";
+const API_URL = 'https://demo.dotcms.com/api/v1/graphql';
 
 const username = process.env.DOTCMS_USERNAME;
 const password = process.env.DOTCMS_PASSWORD;
 
-const AUTH_HEADER = "Basic " + Buffer.from(`${username}:${password}`).toString("base64");
+const AUTH_HEADER = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
 
 // GraphQL Query
 const QUERY = `
@@ -28,30 +28,30 @@ const QUERY = `
 
 
 module.exports = async function () {
-    try {
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: AUTH_HEADER,
-            },
-            body: JSON.stringify({ query: QUERY })
-        });
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: AUTH_HEADER,
+      },
+      body: JSON.stringify({ query: QUERY })
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error fetching data: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-
-        console.log("Fetched Blog Data:", JSON.stringify(data, null, 2));
-
-        return data.data.BlogCollection || [];
-
-    } catch (error) {
-        console.error("Error fetching blog data from dotCMS:", error);
-        return [];
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
     }
+
+    const data = await response.json();
+
+    // console.log('Fetched Blog Data:', JSON.stringify(data, null, 2));
+
+    return data.data.BlogCollection || [];
+
+  } catch (error) {
+        console.error('Error fetching blog data from dotCMS:', error);
+        return [];
+  }
 };
 
 
